c0dc91bb7d69fcac6ea0a82525866b7d674beb81
saszaCodes <sasza.x.ia@gmail.com>
2022-11-04 14:47:17 +0100

    add MSW

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 function App() {
   return (
