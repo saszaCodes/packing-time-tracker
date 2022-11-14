67c78e4fdd2fb8f50204d4ce3417ea113105649c
saszaCodes <sasza.x.ia@gmail.com>
2022-11-14 18:17:33 +0100

    add cors handling and request parsing

--- backend/index.ts
+++ backend/index.ts
@@ -2,13 +2,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { ordersRouter } from "./routes/orders";
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("some res");
-});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(ordersRouter);
 
 app.listen(port);
